687850e14cc7088bd572f7abdd9afdd71838f20f
Rakib Hossan <rakibahmedgraphic@gmail.com>
2020-03-11 19:02:47 +0600

    Ty to clear closure function

---
+++ closure.js
@@ -0,0 +1,28 @@
+//JS CLOSURE Related Discussion
+
+function stopWatch(){
+    let count = 0;
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+const clock1 = stopWatch();
+
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = stopWatch();
+
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock1());
+console.log(clock1());
+
+
+/*
+If i called a function inside a function, then its make a closed environment*/
