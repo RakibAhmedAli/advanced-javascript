e0d348cbc8a1e2cb0419b5a4062544dbefbd9e1d
Rakib Hossan <rakibahmedgraphic@gmail.com>
2020-03-12 10:35:38 +0600

    JS Break & Continue Clear

---
+++ break-continue.js
@@ -0,0 +1,27 @@
+// Break Example
+
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < nums.length; i++){
+    if(nums [i] > 3){
+        break;
+    }
+
+    console.log(nums[i]);
+
+}
+
+// Continue Example
+
+
+const numbers = [1, 2, 3, -4, 5, -6, -7, 8, -9];
+
+for (let i = 0; i < numbers.length; i++){
+    if(numbers [i] < 0){
+        continue;
+    }
+    
+    console.log(numbers[i]);
+
+}
